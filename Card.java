2755d9b0379346533b72b04c23a162fce74bc781
kamcdaniel <ka.mcdaniel12@gmail.com>
2025-04-20 22:02:58 -0700

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,19 @@
+class Card {
+  
+  private int num;
+  private String color;
+
+  public Card(int num, String color) {
+    this.num = num;
+    this.color = color;
+  }
+
+  public int getNum() {
+    return num;
+  }
+
+  public String getColor() {
+    return color;
+  }
+  
+}
